f425ce18325f4418369684e859753600c6ca7383
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-04-29 15:49:20 +0900

    iOSPediaData.json의 imgae -> imgaeName으로 수정, iOSPedia struct에 image, imageName 추가

--- iOSPedia/iOSPedia.swift
+++ iOSPedia/iOSPedia.swift
@@ -6,11 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 struct iOSPedia: Hashable, Codable {
     var id: Int
     var title: String
     var description: String
+    
+    // imageName에는 어떠한 수정이 필요없으므로 private
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
 }
 
