1996e3a1111799a9ad685ce3c7d0035e4c31d732
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-04-29 15:44:48 +0900

    iOSPedia struct 생성

---
+++ iOSPedia/iOSPedia.swift
@@ -0,0 +1,16 @@
+//
+//  iOSPedia.swift
+//  iOSPedia
+//
+//  Created by 이재웅 on 2022/04/29.
+//
+
+import Foundation
+
+
+struct iOSPedia: Hashable, Codable {
+    var id: Int
+    var title: String
+    var description: String
+}
+
