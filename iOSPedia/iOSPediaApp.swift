889a45871f5b39890ae85bdfe698264bbf69e1e3
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-04-29 15:28:36 +0900

    iOSPedia 프로젝트 생성

---
+++ iOSPedia/iOSPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPediaApp.swift
+//  iOSPedia
+//
+//  Created by 이재웅 on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct iOSPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
