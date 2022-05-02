52dd7c06c1202b2b85e0dc317f5d7560487a67d3
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-05-02 22:13:57 +0900

    ContentsView 구현 및 텍스트 전달 구현

---
+++ iOSPedia/ContentsView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentsView.swift
+//  iOSPedia
+//
+//  Created by 이재웅 on 2022/05/02.
+//
+
+import SwiftUI
+
+struct ContentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentsView()
+    }
+}
